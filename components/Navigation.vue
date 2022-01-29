6dac6615a68091a364dd8ca6fc0f4d043558ef25
QuirinEcker <quirin.ecker@bajupa.com>
2022-01-29 16:23:18 +0100

    added navigation hover animation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,7 +2,7 @@
   <div v-if="navigationData !== null" id="navigation" >
     <nav>
       <div id="home">
-        <img :src="require(`${navigationData.home.img}`)"  alt="Logo">
+        <img :src="require(`~/assets/logo.png`)"  alt="Logo">
       </div>
       <div id="navigation-entries">
         <div v-for="navigationEntry in navigationData.entries" :key="navigationEntry.text" class="navigation-entry">
@@ -68,6 +68,34 @@ export default class Navigation extends Vue{
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+}
+
+#navigation #navigation-entries .navigation-entry {
+  height: 100%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  cursor: pointer;
+}
+
+#navigation #navigation-entries .navigation-entry::after {
+  position: absolute;
+  content: '';
+  background: rgb(233 78 27);
+  background: linear-gradient(90deg, rgba(233 78 27 / 100%) 0%, rgba(191 23 34 / 100%) 100%);
+  width: 100%;
+  height: 0.5vh;
+  transform: translateY(15px);
+  border-radius: 30px;
+  opacity: 0;
+  transition: 0.3s;
+}
+
+#navigation #navigation-entries .navigation-entry:hover::after {
+  opacity: 1;
+  transform: translateY(12px);
 }
 
 </style>
