ca6dfd5b8d3234eb1fcffbfbfba98eb13486856e
QuirinEcker <quirin.ecker@bajupa.com>
2022-01-30 20:37:57 +0100

    previes work in progress

--- components/PreviewSegment.vue
+++ components/PreviewSegment.vue
@@ -1,5 +1,24 @@
 <template>
-  <div id="preview-segment"></div>
+  <div id="preview-segment">
+    <div id="dashboard-preview" class="preview">
+      <div class="text">
+
+      </div>
+      <div class="image" :style="{backgroundImage: `url('')`}">
+
+      </div>
+    </div>
+
+    <div id="model3d-preview" class="preview">
+      <div class="text">
+
+      </div>
+
+      <div class="image">
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,7 +31,7 @@ export default class PreviewSegment extends Vue{
 
   created() {
     this.data = this.$nuxt.context
-      .$content('preview/preview-data.json')
+      .$content('preview/preview-data')
       .fetch() as any
   }
 }
@@ -22,5 +41,16 @@ export default class PreviewSegment extends Vue{
 #preview-segment {
   height: 50vh;
   width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+#preview-segment .preview {
+  width: 40%;
+  height: 80%;
+  background-color: white;
+  box-shadow: 0 0 12px -4px #000;
+  border-radius: 20px;
 }
 </style>
