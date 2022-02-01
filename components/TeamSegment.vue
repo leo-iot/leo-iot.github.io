7ea68e6755a1c749dab3b57f19cd9204bcbadebf
QuirinEcker <quirin.ecker@bajupa.com>
2022-02-01 12:02:25 +0100

    background is now dynamic and has a shadow

--- components/TeamSegment.vue
+++ components/TeamSegment.vue
@@ -27,6 +27,7 @@ export default class TeamSegment extends Vue {
 #team-segment {
   position: relative;
   width: 100%;
+  height: auto;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -37,13 +38,14 @@ export default class TeamSegment extends Vue {
   top: 0;
   background: rgb(233 78 27);
   background: linear-gradient(90deg, rgba(233 78 27 / 100%) 0%, rgba(191 23 34 / 100%) 100%);
+  box-shadow: 0 0 12px -4px #000;
   border-radius: 50px 50px 0 0;
-  height: 500px;
   width: 100vw;
+  height: 100%;
 }
 
 #content {
-  position: absolute;
+  position: relative;
   top: 0;
   width: 100%;
   height: 500px;
