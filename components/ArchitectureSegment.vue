58a4f53aea6637efd8ed0dba28efc1980b645d9b
QuirinEcker <quirin.ecker@bajupa.com>
2022-02-03 08:34:06 +0100

    created Architecture Vue and added it to the main page

---
+++ components/ArchitectureSegment.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="architecture-segment">
+    <img src="" alt="architecture">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArchitectureSegment"
+}
+</script>
+
+<style scoped>
+#architecture-segment {
+  width: 100%;
+  height: 500px;
+}
+</style>
